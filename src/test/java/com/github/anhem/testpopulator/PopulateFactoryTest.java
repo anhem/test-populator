89a3139eaa355e25ef70ff57c635b01ff161c7c9
Andreas Hemmingsson <andhem@gmail.com>
2020-11-14 16:56:31 +0100

    added test

--- src/test/java/com/github/anhem/testpopulator/PopulateFactoryTest.java
+++ src/test/java/com/github/anhem/testpopulator/PopulateFactoryTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.util.Collections;
 
 import static com.github.anhem.testpopulator.PopulateFactory.MISSING_STRATEGIES;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class PopulateFactoryTest {
@@ -19,5 +20,12 @@ class PopulateFactoryTest {
         assertThatThrownBy(() -> new PopulateFactory(populateConfig))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(MISSING_STRATEGIES);
+    }
+
+    @Test
+    void canCreatePopulateFactoryWithoutDefaultConfiguration() {
+        PopulateFactory populateFactory = new PopulateFactory();
+
+        assertThat(populateFactory).isNotNull();
     }
 }
