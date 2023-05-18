7a9404212521c42aca8b0f9b589501a519e9c4e6
Andreas Hemmingsson <andhem@gmail.com>
2023-05-18 21:53:41 +0200

    added test

--- src/test/java/com/github/anhem/testpopulator/readme/ReadMeTest.java
+++ src/test/java/com/github/anhem/testpopulator/readme/ReadMeTest.java
@@ -1,14 +1,21 @@
 package com.github.anhem.testpopulator.readme;
 
+import com.github.anhem.testpopulator.ObjectException;
+import com.github.anhem.testpopulator.PopulateException;
 import com.github.anhem.testpopulator.PopulateFactory;
+import com.github.anhem.testpopulator.config.OverridePopulate;
+import com.github.anhem.testpopulator.config.PopulateConfig;
 import com.github.anhem.testpopulator.readme.model.MyClass;
 import com.github.anhem.testpopulator.readme.model.MyClass2;
+import com.github.anhem.testpopulator.readme.model.MyUUID;
 import org.assertj.core.api.Condition;
 import org.junit.jupiter.api.Test;
 
 import java.util.Map;
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ReadMeTest {
 
@@ -31,4 +38,18 @@ class ReadMeTest {
         assertThat(myClass2).extracting("myInnerClass.stringToLocalDateMap").is(new Condition<>(o -> ((Map<?, ?>) o).size() > 0, "not empty"));
     }
 
+    @Test
+    void exceptionIsThrownWhenObjectFactoryIsEnabledAndOverridePopulateImplementationDoesNotImplementCreateString() {
+        OverridePopulate<MyUUID> myUUIDOverridePopulate = () -> new MyUUID(UUID.randomUUID().toString());
+        PopulateFactory populateFactory = new PopulateFactory(
+                PopulateConfig.builder()
+                        .overridePopulate(myUUIDOverridePopulate)
+                        .objectFactoryEnabled(true)
+                        .build()
+        );
+
+        Throwable cause = assertThrows(PopulateException.class, () -> populateFactory.populate(MyClass2.class)).getCause();
+        assertThat(cause.getClass()).isEqualTo(ObjectException.class);
+        assertThat(cause.getMessage()).isEqualTo(String.format("createString() is not implemented in class %s", myUUIDOverridePopulate.getClass().getName()));
+    }
 }
