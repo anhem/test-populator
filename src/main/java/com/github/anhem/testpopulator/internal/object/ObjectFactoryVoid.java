84891b9879ef3da09e2ef8c0b772918a392da2cf
Andreas Hemmingsson <andhem@gmail.com>
2024-10-06 13:02:12 +0200

    updated

--- src/main/java/com/github/anhem/testpopulator/internal/object/ObjectFactoryVoid.java
+++ src/main/java/com/github/anhem/testpopulator/internal/object/ObjectFactoryVoid.java
@@ -5,77 +5,77 @@ import static com.github.anhem.testpopulator.internal.object.ObjectResult.EMPTY_
 public class ObjectFactoryVoid implements ObjectFactory {
     @Override
     public <T> void constructor(Class<T> clazz, int expectedChildren) {
-
+        //ignored
     }
 
     @Override
     public <T> void setter(Class<T> clazz, int expectedChildren) {
-
+        //ignored
     }
 
     @Override
     public <T> void mutator(Class<T> clazz, int expectedChildren) {
-
+        //ignored
     }
 
     @Override
     public <T> void builder(Class<T> clazz, int expectedChildren) {
-
+        //ignored
     }
 
     @Override
     public void method(String methodName, int expectedChildren) {
-
+        //ignored
     }
 
     @Override
     public <T> void set(Class<T> clazz) {
-
+        //ignored
     }
 
     @Override
     public void setOf() {
-
+        //ignored
     }
 
     @Override
     public <T> void list(Class<T> clazz) {
-
+        //ignored
     }
 
     @Override
     public void listOf() {
-
+        //ignored
     }
 
     @Override
     public <T> void map(Class<T> clazz) {
-
+        //ignored
     }
 
     @Override
     public void mapOf() {
-
+        //ignored
     }
 
     @Override
     public <T> void mapEntry(Class<T> clazz) {
-
+        //ignored
     }
 
     @Override
     public <T> void array(Class<T> clazz) {
-
+        //ignored
     }
 
     @Override
     public <T> void value(T value) {
-
+        //ignored
     }
 
     @Override
     public <T> void nullValue(Class<T> clazz) {
-
+        //ignored
     }
 
     @Override
@@ -85,6 +85,6 @@ public class ObjectFactoryVoid implements ObjectFactory {
 
     @Override
     public void writeToFile() {
-
+        //ignored
     }
 }
