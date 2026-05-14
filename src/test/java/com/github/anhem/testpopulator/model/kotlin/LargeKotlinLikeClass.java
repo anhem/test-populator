71e9c5a0a83d79890a2bb1b5ce549420adbb7d98
Andreas Hemmingsson <andreas@hemmingsson.me>
2026-05-14 10:51:05 +0200

    refactor: move LargeKotlinLikeClass and DefaultConstructorMarker to top-level files

---
+++ src/test/java/com/github/anhem/testpopulator/model/kotlin/LargeKotlinLikeClass.java
@@ -0,0 +1,60 @@
+package com.github.anhem.testpopulator.model.kotlin;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+@Getter
+@EqualsAndHashCode
+public class LargeKotlinLikeClass {
+    private final String p01;
+    private final String p02;
+    private final String p03;
+    private final String p04;
+    private final String p05;
+    private final String p06;
+    private final String p07;
+    private final String p08;
+    private final String p09;
+    private final String p10;
+    private final String p11;
+    private final String p12;
+    private final String p13;
+    private final String p14;
+    private final String p15;
+    private final String p16;
+    private final String p17;
+    private final String p18;
+    private final String p19;
+    private final String p20;
+    private final String p21;
+    private final String p22;
+    private final String p23;
+    private final String p24;
+    private final String p25;
+    private final String p26;
+    private final String p27;
+    private final String p28;
+    private final String p29;
+    private final String p30;
+    private final String p31;
+    private final String p32;
+    private final String p33;
+
+    public LargeKotlinLikeClass(String p01, String p02, String p03, String p04, String p05, String p06, String p07, String p08, String p09, String p10,
+                                String p11, String p12, String p13, String p14, String p15, String p16, String p17, String p18, String p19, String p20,
+                                String p21, String p22, String p23, String p24, String p25, String p26, String p27, String p28, String p29, String p30,
+                                String p31, String p32, String p33,
+                                int mask0, int mask1, DefaultConstructorMarker marker) {
+        this.p01 = p01; this.p02 = p02; this.p03 = p03; this.p04 = p04; this.p05 = p05; this.p06 = p06; this.p07 = p07; this.p08 = p08; this.p09 = p09; this.p10 = p10;
+        this.p11 = p11; this.p12 = p12; this.p13 = p13; this.p14 = p14; this.p15 = p15; this.p16 = p16; this.p17 = p17; this.p18 = p18; this.p19 = p19; this.p20 = p20;
+        this.p21 = p21; this.p22 = p22; this.p23 = p23; this.p24 = p24; this.p25 = p25; this.p26 = p26; this.p27 = p27; this.p28 = p28; this.p29 = p29; this.p30 = p30;
+        this.p31 = p31; this.p32 = p32; this.p33 = p33;
+    }
+
+    public LargeKotlinLikeClass(DefaultConstructorMarker marker) {
+        this.p01 = null; this.p02 = null; this.p03 = null; this.p04 = null; this.p05 = null; this.p06 = null; this.p07 = null; this.p08 = null; this.p09 = null; this.p10 = null;
+        this.p11 = null; this.p12 = null; this.p13 = null; this.p14 = null; this.p15 = null; this.p16 = null; this.p17 = null; this.p18 = null; this.p19 = null; this.p20 = null;
+        this.p21 = null; this.p22 = null; this.p23 = null; this.p24 = null; this.p25 = null; this.p26 = null; this.p27 = null; this.p28 = null; this.p29 = null; this.p30 = null;
+        this.p31 = null; this.p32 = null; this.p33 = null;
+    }
+}
