ed01685e9271c4de7bc6ff64ba9bad7bce6d703b
Andreas Hemmingsson <andreas@hemmingsson.me>
2026-06-14 12:49:54 +0200

    refactoring

---
+++ src/test/java/com/github/anhem/testpopulator/model/java/field/FieldPrivateConstructor.java
@@ -0,0 +1,12 @@
+package com.github.anhem.testpopulator.model.java.field;
+
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@EqualsAndHashCode
+public class FieldPrivateConstructor {
+}
